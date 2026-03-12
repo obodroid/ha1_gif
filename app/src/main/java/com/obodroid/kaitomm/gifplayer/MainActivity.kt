0b5ceb34ae8690b848db7f1fc1e3ed4e76ad1e18
Jiradej Vatanutanon <jiradej.vat@velo.org>
2026-03-12 17:16:38 +0700

    clean up UI

--- app/src/main/java/com/obodroid/kaitomm/gifplayer/MainActivity.kt
+++ app/src/main/java/com/obodroid/kaitomm/gifplayer/MainActivity.kt
@@ -4,31 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.google.gson.Gson
 import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var recyclerView: RecyclerView
     private lateinit var gifImageView: ImageView
-    private lateinit var currentEmotionText: TextView
     private lateinit var emotions: List<Emotion>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerView = findViewById(R.id.recyclerView)
         gifImageView = findViewById(R.id.gifImageView)
-        currentEmotionText = findViewById(R.id.currentEmotionText)
 
         loadEmotions()
-        setupRecyclerView()
 
         handleIntent(intent)
     }
@@ -51,15 +43,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun setupRecyclerView() {
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = EmotionAdapter(emotions) { emotion ->
-            showGif(emotion)
-        }
-    }
-
     private fun showGif(emotion: Emotion) {
-        currentEmotionText.text = emotion.emotion
         val resourceId = resources.getIdentifier(emotion.filename, "raw", packageName)
         if (resourceId != 0) {
             Glide.with(this)
